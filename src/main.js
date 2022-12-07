77ea6c05a2279922e12620395175872f727eafac
Paradox021 <espanacarmelo@gmail.com>
2022-12-07 14:11:55 +0100

    refactor

--- src/main.js
+++ src/main.js
@@ -7,19 +7,16 @@ document.getElementById("buscar").addEventListener("click",async () => {
     const formas = [...document.getElementsByTagName('input')]
         .filter(input => input.checked)
         .map(checked => checked.value)
-    const selectSombrero = document.getElementById("sombreroInf")
-    const selectEsporas = document.getElementById("esporas")
-    
-    const sombreroValue = selectSombrero.options[selectSombrero.selectedIndex].value
-    const esporasValue = selectEsporas.options[selectEsporas.selectedIndex].value
+    const sombreroValue = document.querySelector("#sombreroInf").value
+    const esporasValue = document.querySelector("#esporas").value
 
-    const aux = Object.entries(await myshroom)
-        .filter(shroom => shroom[1].attributes.sporePrint.indexOf(esporasValue)!=-1)
-        .filter(shroom => shroom[1].attributes.hymenium.indexOf(sombreroValue)!=-1)
-        .filter(shroom => compruebaElementos(shroom[1].attributes.cap, formas))
-        .map(shroom => `<div>Nombre: ${shroom[1].common[0]}<br>
-            Venenosa: ${shroom[1].attributes.poisonous&&'Si'||'No'}<br>
-            Mortal: ${shroom[1].attributes.deadly&&'Si'||'No'}</div>`)
+    const aux = (await myshroom)
+        .filter(shroom => shroom.attributes.sporePrint.indexOf(esporasValue)!=-1)
+        .filter(shroom => shroom.attributes.hymenium.indexOf(sombreroValue)!=-1)
+        .filter(shroom => compruebaElementos(shroom.attributes.cap, formas))
+        .map(shroom => `<div>Nombre: ${shroom.common[0]}<br>
+            Venenosa: ${shroom.attributes.poisonous&&'Si'||'No'}<br>
+            Mortal: ${shroom.attributes.deadly&&'Si'||'No'}</div>`)
     document.getElementById('info').innerHTML=aux.join('')||'No existen setas que cumplan el filtro'
 
 } )
